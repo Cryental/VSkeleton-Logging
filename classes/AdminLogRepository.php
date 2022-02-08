d1cc29213a55b72fe1f92e57f00fb0ea780468c0
Scorias <mostafa.samy203@gmail.com>
2022-02-08 18:05:40 +0200

    fix query bugs and add more retrieval methods

--- classes/AdminLogRepository.php
+++ classes/AdminLogRepository.php
@@ -12,4 +12,30 @@ class AdminLogRepository
             'user_agent' => $inputs['user_agent'],
         ]);
     }
+
+    public function Find($log_id)
+    {
+        return AdminLog::query()->where('id', $log_id)->first();
+    }
+
+    public function FindAll($needle, $page, $limit)
+    {
+        $columns = [
+            'id',
+            'access_token_id',
+            'url',
+            'method',
+            'ip',
+            'user_agent',
+            'created_at'
+        ];
+
+        $query = AdminLog::query();
+
+        foreach ($columns as $column) {
+            $query->orWhere("$column", 'LIKE', "%$needle%");
+        }
+        return $query->orderBy('created_at', 'DESC')
+            ->get();
+    }
 }
--- classes/UserLogRepository.php
+++ classes/UserLogRepository.php
@@ -5,11 +5,60 @@ class UserLogRepository
     public function Create(array $inputs)
     {
         UserLog::query()->create([
-            'personal_token_id' => $inputs['personal_token_id'],
+            'subscription_id' => $inputs['subscription_id'],
             'url' => $inputs['url'],
             'ip' => $inputs['ip'],
             'method' => $inputs['method'],
             'user_agent' => $inputs['user_agent'],
         ]);
+    }
+
+    public function Find($log_id)
+    {
+        return UserLog::query()->where('id', $log_id)->first();
+    }
+
+    public function FindAll($needle, $page, $limit)
+    {
+        $columns = [
+            'id',
+            'subscription_id',
+            'url',
+            'method',
+            'ip',
+            'user_agent',
+            'created_at'
+        ];
+
+        $query = UserLog::query();
+
+        foreach ($columns as $column) {
+            $query->orWhere("user_logs.$column", 'LIKE', "%$needle%");
+        }
+        return $query->orderBy('user_logs.created_at', 'DESC')->get();
+    }
+
+
+    public function FindLogsBySubscription($subscription_id, $needle, $page, $limit)
+    {
+        return UserLog::where('subscription_id', $subscription_id)->where(function($q) use($needle){
+            $q->where('id', 'LIKE', "%$needle%")
+                ->orWhere('subscription_id', 'LIKE', "%$needle%")
+                ->orWhere('url', 'LIKE', "%$needle%")
+                ->orWhere('method', 'LIKE', "%$needle%")
+                ->orWhere('ip', 'LIKE', "%$needle%")
+                ->orWhere('user_agent', 'LIKE', "%$needle%")
+                ->orWhere('created_at', 'LIKE', "%$needle%");
+        })->orderBy('user_logs.created_at', 'DESC')
+            ->get();
+    }
+
+
+    public function FindLogsBySubscriptionCount($subscription_id, $date): int
+    {
+        return UserLog::where('subscription_id', $subscription_id)
+            ->whereMonth('user_logs.created_at', $date->format('m'))
+            ->whereYear('user_logs.created_at', $date->format('Y'))
+            ->count();
     }
 }
