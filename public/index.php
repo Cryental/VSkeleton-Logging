ef54168002e571ad2d50d7f0804dd9c7e9c055d7
Scorias <mostafa.samy203@gmail.com>
2022-02-08 18:05:57 +0200

    added routes

--- public/index.php
+++ public/index.php
@@ -11,8 +11,7 @@ Flight::map('notFound', function(){
 
 Flight::route('POST /logs/admins', function () {
     try {
-        $tokenRepo = new AccessTokenRepository();
-        if (!$tokenRepo->AuthAccessToken(AuthHelper::GetBearerToken())){
+        if (!AuthHelper::Auth()){
             http_response_code(401);
             exit('');
         }
@@ -33,19 +32,48 @@ Flight::route('POST /logs/admins', function () {
         exit('');
     }
 });
+Flight::route('GET /logs/admins/', function (){
+    try {
+        if (!AuthHelper::Auth()){
+            http_response_code(401);
+            exit('');
+        }
+
+        $needle = Flight::request()->query['search']?? '';
+
+        // Find solution to pagination (doesnt work now)
+        $page = Flight::request()->query['page']?? 1;
+        $limit= Flight::request()->query['limit']?? 50;
+
+        $adminLogRepo = new AdminLogRepository();
+        $logs = $adminLogRepo->FindAll(
+            $needle,
+            $page,
+            $limit
+        );
+
+        http_response_code(200);
+        Flight::json($logs);
+        exit('');
+    }
+    catch (Exception $ex){
+        ray($ex->getMessage());
+        http_response_code(500);
+        exit('');
+    }
+});
+
 
 Flight::route('POST /logs/users', function () {
     try {
-        $tokenRepo = new AccessTokenRepository();
-
-        if (!$tokenRepo->AuthAccessToken(AuthHelper::GetBearerToken())){
+        if (!AuthHelper::Auth()){
             http_response_code(401);
             exit('');
         }
 
         $userLogRepo = new UserLogRepository();
         $userLogRepo->Create([
-            'personal_token_id' => Flight::request()->data->personal_token_id,
+            'subscription_id' => Flight::request()->data->subscription_id,
             'url' => Flight::request()->data->url,
             'ip' => Flight::request()->data->ip,
             'method' => Flight::request()->data->method,
@@ -55,9 +83,88 @@ Flight::route('POST /logs/users', function () {
         exit('');
     }
     catch (Exception $ex){
+        ray($ex->getMessage());
         http_response_code(500);
         exit('');
     }
 });
+Flight::route('GET /logs/users/@subscription_id/count', function ($subscription_id) {
+    try {
+        if (!AuthHelper::Auth()){
+            http_response_code(401);
+            exit('');
+        }
+
+        $userLogRepo = new UserLogRepository();
+        $count = $userLogRepo->FindLogsBySubscriptionCount($subscription_id,\Carbon\Carbon::now());
+        http_response_code(200);
+        Flight::json($count);
+        exit('');
+    }
+    catch (Exception $ex){
+        http_response_code(500);
+        exit('');
+    }
+});
+Flight::route('GET /logs/users/@subscription_id/', function ($subscription_id){
+    try {
+        if (!AuthHelper::Auth()){
+            http_response_code(401);
+            exit('');
+        }
+        $needle = Flight::request()->query['search']?? '';
+
+        // Find solution to pagination (doesnt work now)
+        $page = Flight::request()->query['page']?? 1;
+        $limit= Flight::request()->query['limit']?? 50;
+
+        $userLogRepo = new UserLogRepository();
+        $logs = $userLogRepo->FindLogsBySubscription($subscription_id,
+            $needle,
+            $page,
+            $limit
+        );
+
+        http_response_code(200);
+        Flight::json($logs);
+        exit('');
+    }
+    catch (Exception $ex){
+        http_response_code(500);
+        exit('');
+    }
+});
+Flight::route('GET /logs/users/', function (){
+    try {
+        if (!AuthHelper::Auth()){
+            http_response_code(401);
+            exit('');
+        }
+
+        $needle = Flight::request()->query['search']?? '';
+
+        // Find solution to pagination (doesnt work now)
+        $page = Flight::request()->query['page']?? 1;
+        $limit= Flight::request()->query['limit']?? 50;
+
+        $userLogRepo = new UserLogRepository();
+        $logs = $userLogRepo->FindAll(
+            $needle,
+            $page,
+            $limit
+        );
+
+        http_response_code(200);
+        Flight::json($logs);
+        exit('');
+    }
+    catch (Exception $ex){
+        http_response_code(500);
+        exit('');
+    }
+});
+
+
+
 
 Flight::start();	
