45b564374b9116aca3791586769fb0ad3c542a98
Scorias <mostafa.samy203@gmail.com>
2022-02-24 19:31:26 +0200

    Adding DTO structure

---
+++ DataTransferObjects/AdminLogDTO.php
@@ -0,0 +1,34 @@
+<?php
+
+class AdminLogDTO extends DataTransferObjectBase
+{
+    public string $access_token_id;
+    public string $logging_access_token_id;
+
+    public string $id;
+    public string $url;
+    public string $ip;
+    public string $method;
+    public ?string $user_agent;
+    public string $created_at;
+
+    public static function fromModel($userLog): self
+    {
+        return new self($userLog);
+    }
+
+    public function GetDTO(): array
+    {
+        return [
+            'id'           => $this->id,
+            'access_token' => [
+                'id' => $this->access_token_id,
+            ],
+            'url'        => $this->url,
+            'ip'         => $this->ip,
+            'method'     => $this->method,
+            'user_agent' => $this->user_agent,
+            'created_at' => $this->created_at
+        ];
+    }
+}
